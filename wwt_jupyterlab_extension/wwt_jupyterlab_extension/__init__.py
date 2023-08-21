b403a03664f545be8846089d425dbc01c23ee624
Peter Williams <peter@newton.cx>
2023-08-21 11:20:52 -0400

    wwt_jupyterlab_extension/wwt_jupyterlab_extension/__init__.py: streamline this

--- wwt_jupyterlab_extension/wwt_jupyterlab_extension/__init__.py
+++ wwt_jupyterlab_extension/wwt_jupyterlab_extension/__init__.py
@@ -1,18 +1,10 @@
-# Copyright 2021 the .NET Foundation
+# Copyright 2021-2023 the .NET Foundation
 # Licensed under the MIT License
 
-import json
-import os.path as osp
-
 from ._version import __version__
-
-HERE = osp.abspath(osp.dirname(__file__))
-
-with open(osp.join(HERE, 'labextension', 'package.json')) as fid:
-    data = json.load(fid)
 
 def _jupyter_labextension_paths():
     return [{
         'src': 'labextension',
-        'dest': data['name']
+        'dest': '@wwtelescope/jupyterlab',
     }]
